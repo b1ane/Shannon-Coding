f5ad19376b979d9d793d0a6cbe26d592a765aff7
Blane Zewdie <b1ane@outlook.com>
2024-09-03 12:10:56 -0500

    Read the n lines from STDIN

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
 //Write your program here
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main() {
+    std::string line;
+    std::vector<std::string> lines;
+
+    std::cout << "Enter lines of text: " << std::endl;
+
+    while(std::getline(std::cin, line)) {
+        if(line.empty()) {
+            break;
+        }
+        lines.push_back(line);
+    }
+
+    for(int i = 0; i < lines.size(); i++) {
+        std::cout << lines.at(i) << std::endl;
+    }
+
+    return 0;
+}
