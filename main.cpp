8ab2a97aded75298f0e74d5a861ee13170adaca5
Blane Zewdie <b1ane@outlook.com>
2024-09-05 16:49:16 -0500

    Outputs input message

--- main.cpp
+++ main.cpp
@@ -2,23 +2,67 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <pthread.h>
 
+
+struct Data {
+    char letter;
+    int frequency;
+    std::string line;
+};
+
+
+void* freq(void* void_ptr) {
+    Data* ptr = (Data*) void_ptr;
+
+    return ptr;
+}
+
+//main thread reads input
 int main() {
     std::string line;
-    std::vector<std::string> lines;
+    std::vector<Data> lines;
+
+    Data temp;
 
     std::cout << "Enter lines of text: " << std::endl;
-
     while(std::getline(std::cin, line)) {
         if(line.empty()) {
             break;
         }
-        lines.push_back(line);
+
+        temp.line = line; //stores line in the Data structure 
+        temp.letter = '\0'; //initializes letter 
+        temp.frequency = 0; //initializes frequency 
+
+        lines.push_back(temp); //adds struct instance to vector
     }
 
-    for(int i = 0; i < lines.size(); i++) {
-        std::cout << lines.at(i) << std::endl;
+    int numLines = lines.size();
+    pthread_t* tid = new pthread_t[numLines];
+
+    for( int i= 0; i < numLines; i++) {
+        if(pthread_create(&tid[i], nullptr, freq, (void*) &lines.at(i)) != 0) {
+            std::cerr << "Error creating thread" << std::endl;
+            return 1;
+        }
     }
+
+    for( int i = 0; i < numLines; i++) {
+        pthread_join(tid[i],nullptr);
+    }
+
+    for( int i = 0; i < numLines; i++ ) {
+        std::cout << "Message: " << lines[i].line << std::endl;
+    }
+
+    if(tid!=nullptr){
+        delete[] tid;
+    }
+
+
+
+
 
     return 0;
 }
